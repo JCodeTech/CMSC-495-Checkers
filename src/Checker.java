034dc1e7ff375b9931873489d8493eb45cf40aee
AirQual1ty <115928138+AirQual1ty@users.noreply.github.com>
2026-06-23 15:07:22 +0200

    Add files via upload

---
+++ src/Checker.java
@@ -0,0 +1,34 @@
+/**  
+* UMGC CMSC 495
+* Illustrates incremental program development 
+* Class Checker - Class that stores information about a single piece on the checkerboard.
+* @author Alexander Egan
+* Date: June 22, 2026
+* JavaJDK - 26
+*/
+
+public class Checker {
+
+	private boolean isKing = false;
+	private Color color;
+	
+	public Checker (String color) {
+		this.color = new Color(color);
+	}
+	
+	public void checkerKing() {
+		isKing = true;
+	}
+	
+	public boolean isKing() {
+		return isKing;
+	}
+	
+	public String getColor() {
+		return color.getColor();
+	}
+	
+	public String toString() {
+		return "" + color;
+	}
+}
---
+++ src/GameException.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class GameException extends IOException{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public GameException (String message) {
+		super(message);
+	}
+}
