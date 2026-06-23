4b47317e68d5a508490439d9312b6a875b3ae9c3
AirQual1ty <115928138+AirQual1ty@users.noreply.github.com>
2026-06-23 15:09:14 +0200

    Update UnitTests.java

--- src/UnitTests.java
+++ src/UnitTests.java
@@ -1,3 +1,12 @@
+/**  
+* UMGC CMSC 495
+* Illustrates incremental program development 
+* Class Checker - Class that stores the current state of the checkerboard. The board can be updated as needed.
+* @author Alexander Egan
+* Date: June 22, 2026
+* JavaJDK - 26
+*/
+
 import java.io.IOException;
 
 public class UnitTests {
