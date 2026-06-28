4e7b00f3ed476a904b38fdbb3eeef3b59b6018c2
Joseph Romano <josephromano2015@outlook.com>
2026-06-27 23:34:11 -0700

    Update Player class with constructor, checker management methods, and documentation

--- src/com/game/entities/Player.java
+++ src/com/game/entities/Player.java
@@ -1,29 +1,43 @@
-package com.game.entities;
-/*
-CMSC 495 Computer Science Capstone
-Group 2 (Joseph Romano, Alexander Egan, Pavel Parada)
-File Author: Joseph Romano
-Modified By (list):
-* Joseph Romano
+/**
+ * UMGC CMSC 495
+ * Developers: Joseph Romano
+ * Date: June 2026
+ * JavaJDK - 26
  */
 
+package com.game.entities;
+
 public class Player {
+    private String name; // Player's name
     private String color; // "RED" or "BLACK"
     private int Pieces;
     private boolean isHuman; // true if human player, false if AI player
 
-    public Player(String color) {
-        this.color = color;
-        this.Pieces = 12;
+    public Player(String playerName, String color) {
+        this.name = "Player"; // Default name, will be changed when a new player is created. i.e. "Player " + 1 or 2
+        this.color = color; // Red or Black
+        this.Pieces = 0; // When the board is set up the count is handled in that class via Player player1.addChecker.
     }
 
-    public void removePiece() {
+    // Used to remove a checker from total checker count for that player when captured.
+    public void removeChecker() {
         if (Pieces > 0) {
             Pieces--;
         }
     }
 
+    // Used during board set up to add to the player counter.
+    public void addChecker(){
+        Pieces++;
+    }
+
+    // Used for capture validation (Maybe handled in the checker class) Though we can use this for side / turn validation.
     public String getColor() {
         return color;
+    }
+
+    // Gets player's name
+    public String getPlayerName(){
+        return name;
     }
 }
