9a93a3bfbec519707b068698d98f5864afb71b47
Joseph Romano <josephromano2015@outlook.com>
2026-06-27 21:10:46 -0700

    Update Checker class to simplify constructor and improve method clarity

--- src/com/game/objects/Checker.java
+++ src/com/game/objects/Checker.java
@@ -9,47 +9,32 @@ import java.awt.*;
  * @author Alexander Egan
  * Date: June 22, 2026
  * JavaJDK - 26
+ *
+ * Modified By Romano, Joseph on 06/24/2026
  */
 
 public class Checker {
 
+	// Variables that this class will need
+	private final String color;
 	private boolean isKing = false;
-	private Color color;
-	private int[] placement;
 
-	public Checker (String color, int[] placement) {
-		this.color = new Color(color);
-		this.placement = placement;
+	public Checker (String color, boolean isKing) {
+		this.color = color;
+		this.isKing = false; // All checkers start as regular pieces, not kings
+
 	}
 
-	public void checkerKing() {
-		isKing = true;
+	public void setCheckerKing() {
+		this.isKing= true; // Sets the checker to king status when called.
 	}
 
 	public boolean isKing() {
-		return isKing;
+		return this.isKing; // Returns a boolean value based on if the checker is king or not.
 	}
 
 	public String getColor() {
-		return color.getColor();
-	}
-
-	public String toString() {
-		return "" + color;
-	}
-
-	public int[] getCheckersPlacement(){
-		return placement;
-	}
-
-	// Note from Alexander: I think with the adjustments I made to the getCheckersPlacement, we can say this method is deprecated and remove it.
-	public Checker getTile(int row, int column) {
-		return checkerBoard[row][column];
-	}
-
-	// Makes a checker piece into a king piece.
-	public void checkerKing(int pieceRow, int pieceColumn) {
-		checkerBoard[pieceRow][pieceColumn].checkerKing();
+		return this.color; // Returns the color of the checker.
 	}
 
 }
