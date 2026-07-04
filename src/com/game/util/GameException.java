38b5884e8d5dbe2824dd24faa2cc4a2db39d771e
Joseph Romano <josephromano2015@outlook.com>
2026-07-04 00:30:17 -0700

    Remove unused serialVersionUID from GameException class

--- src/com/game/util/GameException.java
+++ src/com/game/util/GameException.java
@@ -12,11 +12,6 @@ import java.io.IOException;
 
 public class GameException extends IOException{
 
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
-
     public GameException (String message) {
         super(message);
     }
