75f111410a9012c425fadfac3b8e1c2fdbea5d68
Joseph Romano <josephromano2015@outlook.com>
2026-07-04 00:31:54 -0700

    Update Main class to initialize game board and UI components

--- src/com/game/core/Main.java
+++ src/com/game/core/Main.java
@@ -1,20 +1,27 @@
 package com.game.core;
 
+import com.game.objects.Board;
+import com.game.ui.BoardUI;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage primaryStage) {
+
         Window window = new Window();
-        window.init(stage);
+        window.init(primaryStage);
+
+        Board board = new Board();
+        board.newGame();
+
+        BoardUI boardUI = new BoardUI(window.getPane(), board);
+
     }
 
     public static void main(String[] args) {
-
         launch(args);
     }
 }
