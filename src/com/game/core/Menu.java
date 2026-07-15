2307c1007ef9a300463e60639c5c65366523b7fd
AirQual1ty <115928138+AirQual1ty@users.noreply.github.com>
2026-07-15 09:26:59 +0200

    Add files via upload

--- src/com/game/core/Menu.java
+++ src/com/game/core/Menu.java
@@ -1,5 +1,47 @@
+/**
+ * UMGC CMSC 495
+ * Developers: Alexander Egan
+ * Date: June 2026
+ * JavaJDK - 26
+ */
+
 package com.game.core;
 
-public class Menu{
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 
+public class Menu extends Pane {
+    
+    public Menu() {
+    	setStyle("-fx-background-color: #C6C6C6");
+    	setOpacity(0.9);
+    	setPrefSize(800, 800);
+    	
+    	// Game Title Label.
+    	Label checkersLabel = new Label("Checkers");
+    	checkersLabel.setPrefSize(250, 50);
+    	checkersLabel.setLayoutX(300);
+    	checkersLabel.setLayoutY(50);
+    	checkersLabel.isVisible();
+    	checkersLabel.setStyle("-fx-font: 50 arial");
+    	
+    	getChildren().add(checkersLabel);
+    	
+    	Button playerStart = new Button("Player vs Player");
+    	playerStart.setPrefSize(400, 50);
+    	playerStart.setLayoutX(200);
+    	playerStart.setLayoutY(300);
+    	playerStart.setStyle("-fx-font: 30 arial");
+    	
+    	getChildren().add(playerStart);
+    	
+    	Button computerStart = new Button("Player vs Computer");
+    	computerStart.setPrefSize(400, 50);
+    	computerStart.setLayoutX(200);
+    	computerStart.setLayoutY(400);
+    	computerStart.setStyle("-fx-font: 30 arial");
+    	
+    	getChildren().add(computerStart);
+    }
 }
