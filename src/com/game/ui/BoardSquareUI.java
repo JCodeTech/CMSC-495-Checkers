e83f1613ca737db61af50e06094dbb52a8b478e7
Joseph Romano <josephromano2015@outlook.com>
2026-07-18 00:21:12 -0700

    Addeed square selection and deselection

--- src/com/game/ui/BoardSquareUI.java
+++ src/com/game/ui/BoardSquareUI.java
@@ -1,28 +1,35 @@
 package com.game.ui;
 
+import com.game.objects.Board;
 import com.game.objects.BoardSquare;
+import com.game.objects.Checker;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class BoardSquareUI {
+    private Board board;
     private final BoardSquare boardSquare; // The Square object that this UI object will represent.
     private final Rectangle square; // UI representation of the Square.
     private final int row;
     private final int col;
     private static final int squareSize = 60; // Sizes of the board squares
 
-    public BoardSquareUI(BoardSquare boardSquare, int row, int col){
+    public BoardSquareUI(BoardSquare boardSquare, Board board, int row, int col){
         this.boardSquare = boardSquare;
+        this.board = board;
         this.row = row;
         this.col = col;
-
-        // Create the circle here for better encapsulation
         this.square = createSquare();
 
-        // Click handler to reference this specific checker UI object.
         square.setOnMouseClicked(event -> {
             // TODO: Implement Checker Object Linking
             System.out.println("TEST: The " + boardSquare.getColor() + " Square at position (" + row + ", " + col + ") has been clicked on.");
+            BoardSquare selectedSpace = board.findSelected(boardSquare);
+            if (selectedSpace != null) {
+                System.out.println("Found selected object at " + selectedSpace.getRow() + ", " + selectedSpace.getCol());
+                selectedSpace.setSelected(false);
+            }
+            boardSquare.setSelected(true);
         });
     }
 
