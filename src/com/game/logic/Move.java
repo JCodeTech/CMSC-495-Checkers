fe12e0409db33e7ae54d5fcff6a7b3369d091722
Joseph Romano <josephromano2015@outlook.com>
2026-07-19 20:25:02 -0700

    Refactor Move class to utilize MoveValidation and BoardUI

--- src/com/game/logic/Move.java
+++ src/com/game/logic/Move.java
@@ -7,85 +7,51 @@
 
 package com.game.logic;
 
-import com.game.entities.Player;
 import com.game.objects.Board;
 import com.game.objects.BoardSquare;
 import com.game.objects.Checker;
 import com.game.ui.BoardUI;
 import com.game.util.GameException;
 
-/* Example of how this class is used:
-    // When user clicks to move a piece
-    int fromRow = 2, fromCol = 3;
-    int toRow = 4, toCol = 5;
-
-    // Create MoveValidation object with the coordinates
-    MoveValidation move = new MoveValidation(fromRow, fromCol, toRow, toCol);
-
-    // Validate the move
-    boolean isValid = move.validateMove(currentBoard, currentPlayer);
- */
 public class Move {
-    private final int fromRow;
-    private final int fromCol;
-    private final int toRow;
-    private final int toCol;
-    private boolean isCapture;
     private final Board board;
-    private BoardUI checkerUI;
-    private MoveValidation move;
-    private MoveValidation checkMove;
+    private final BoardUI boardUI;
+    private final MoveValidation moveValidation;
 
-    public Move(Board board, int fromRow, int fromCol, int toRow, int toCol) {
+    public Move(Board board, BoardUI boardUI) {
         this.board = board;
-        this.fromRow = fromRow;
-        this.fromCol = fromCol;
-        this.toRow = toRow;
-        this.toCol = toCol;
-    }
-
-    // Method to move a checker from one position to another
-    public void moveChecker(Checker piece, BoardSquare space) {
-        Checker[][] checkerBoard = board.getCheckerPiece(); // Gets the current state of the board
-
-        // Remove piece from source position (set to null)
-        board.clearPosition(fromRow, fromCol);
-
-        // Place piece at destination position (For Board Class, NEEDS REFACTORING!!!!)
-        checkerBoard[toRow][toCol] = piece;
-        piece.setNewPos(space.getRow(), space.getCol());
-
-        checkForKingPromotion(piece);
-        checkerUI.updateCheckerPieceUI();
-    }
-
-    // Check if a piece should be promoted to king
-    private void checkForKingPromotion(Checker piece) {
-        if (piece.getRow() == 0 || piece.getRow() == 7) { // Top or bottom of the board
-            board.promoteToKing(piece.getRow(), piece.getCol());
-        }
-    }
-
-    // Handle capture logic
-    private void handleCapture(int fromRow, int fromCol, int toRow, int toCol) {
-        // Calculate middle position
-        int midRow = fromRow + (toRow - fromRow) / 2;
-        int midCol = fromCol + (toCol - fromCol) / 2;
-
-        // Clear the captured piece
-        board.clearPosition(midRow, midCol);
+        this.boardUI = boardUI;
+        this.moveValidation = new MoveValidation(board);
     }
 
     public void makeMove() throws GameException {
-        BoardSquare space = (BoardSquare) board.findSelected("space");
+        BoardSquare destination = (BoardSquare) board.findSelected("space");
         Checker piece = (Checker) board.findSelected("checker");
 
-        boolean isValid = move.validateMove(piece, space);
+        moveValidation.validateMove(piece, destination);
 
-        if (isValid) {
-            moveChecker(piece, space);
+        int fromRow = piece.getRow();
+        int fromCol = piece.getCol();
+        int toRow = destination.getRow();
+        int toCol = destination.getCol();
+
+        if (Math.abs(toRow - fromRow) == 2 && Math.abs(toCol - fromCol) == 2) {
+            int capturedRow = (fromRow + toRow) / 2;
+            int capturedCol = (fromCol + toCol) / 2;
+            board.clearPosition(capturedRow, capturedCol);
         }
+
+        board.clearPosition(fromRow, fromCol);
+        board.getCheckerPiece()[toRow][toCol] = piece;
+        piece.setNewPos(toRow, toCol);
+
+        if (toRow == 0 || toRow == 7) {
+            board.promoteToKing(toRow, toCol);
+        }
+
+        piece.setSelected(false);
+        destination.setSelected(false);
+
+        boardUI.updateCheckerPieceUI();
     }
-
-
 }
