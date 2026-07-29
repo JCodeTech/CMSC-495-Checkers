b2054cd7b1dfe18ecd409e59729917da5522859a
JCodeTech <josephromano2015@outlook.com>
2026-07-28 21:47:29 -0600

    Intergrated main menu with game

--- src/com/game/core/Main.java
+++ src/com/game/core/Main.java
@@ -3,6 +3,7 @@ package com.game.core;
 import com.game.objects.Board;
 import com.game.ui.BoardUI;
 import javafx.application.Application;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import com.game.entities.Player;
 
@@ -15,14 +16,20 @@ public class Main extends Application {
         Window window = new Window();
         window.init(primaryStage);
 
+        Pane root = window.getPane();
+
+        Controller controller = new Controller();
+
         Board board = new Board();
         board.newGame();
+
         Player player1 = new Player("Player 1", "Red", true);
         Player player2 = new Player("Player 2", "Black", true);
 
+        BoardUI boardUI = new BoardUI(root, board);
 
-        BoardUI boardUI = new BoardUI(window.getPane(), board);
-
+        Menu menu = new Menu();
+        root.getChildren().add(menu);
     }
 
     public static void main(String[] args) {
