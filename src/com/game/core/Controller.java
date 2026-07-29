30d13fdc2c045d2df4e16e6183b82cfa57f92802
JCodeTech <josephromano2015@outlook.com>
2026-07-28 21:45:23 -0600

    Added Controller logic to track game mode and player turn

--- src/com/game/core/Controller.java
+++ src/com/game/core/Controller.java
@@ -1,8 +1,22 @@
 package com.game.core;
 
 public class Controller{
-    boolean player1Turn = true;
-public void flipTurn(){
+
+    public static boolean player1Turn = true;
+    public static boolean playerVsPlayer = true;
+
+public static void flipTurn(){
+
     player1Turn = !player1Turn;
+    System.out.println("It is now " + (player1Turn ? "Player 1's Turn." : "Player 2's Turn."));
+}
+
+public static void setGameMode(boolean gameMode){
+    playerVsPlayer = gameMode;
+    System.out.println("Player vs Player variable is set to: " + gameMode);
+}
+
+public static boolean getPlayerTurn(){
+    return player1Turn;
 }
 }
