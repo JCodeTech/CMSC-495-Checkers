21e0f55ac839d18ff67804ce585366c2253e7511
JCodeTech <josephromano2015@outlook.com>
2026-08-01 18:29:47 -0600

    Added delay for AI turn execution for better gameplay experience

--- src/com/game/core/Controller.java
+++ src/com/game/core/Controller.java
@@ -3,6 +3,8 @@ package com.game.core;
 import com.game.logic.AILogic;
 import com.game.ui.InfoPanel;
 import com.game.util.GameException;
+import javafx.animation.PauseTransition;
+import javafx.util.Duration;
 
 
 public class Controller{
@@ -16,13 +18,24 @@ public static void flipTurn() throws GameException {
 
     player1Turn = !player1Turn;
     System.out.println("It is now " + (player1Turn ? "Player 1's Turn." : "Player 2's Turn."));
+
     if (player1Turn){
         InfoPanel.setTurn("Red");
     }
     else{
         InfoPanel.setTurn("Black");
         if (!playerVsPlayer){
-            AILogic.AITurn();
+            if(!Controller.player1Turn){
+                PauseTransition aiThinking = new PauseTransition(Duration.seconds(1));
+                aiThinking.setOnFinished(e -> {
+                    try {
+                        AILogic.AITurn();
+                    } catch (GameException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                });
+                aiThinking.play();
+            }
         }
     }
 
